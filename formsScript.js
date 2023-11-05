8a98dc00410966f286232680cd502ff7110aad81
abdallah <abdallahradfi@gmail.com>
2023-11-06 00:30:53 +0100

    fixed bugs

--- formsScript.js
+++ formsScript.js
@@ -2,25 +2,25 @@
 const loginBtn = document.querySelector("#login");
 const signupBtn = document.querySelector("#signup");
 
-//* shows the login form and hides the signup form
+// shows the login form and hides the signup form
 const showLoginForm = () => {
     document.getElementById("loginPart").style.display = "block";
     document.getElementById("signupPart").style.display = "none";
 }
 
-//* shows the signup form and hides the login form
+// shows the signup form and hides the login form
 const showSignupForm = () => {
     document.getElementById("loginPart").style.display = "none";
     document.getElementById("signupPart").style.display = "block";
 }
 
-//* adds to the button the selected style, and removes the unselected
+// adds to the button the selected style, and removes the unselected
 const addStyle = (element) => {
     element.classList.add("selected-control-btn");
     element.classList.remove("unselected-control-btn");
 }
 
-//* adds to the button the unselected style, and removes the selected
+// adds to the button the unselected style, and removes the selected
 const removeStyle = (element) => {
     element.classList.add("unselected-control-btn");
     element.classList.remove("selected-control-btn");
@@ -37,24 +37,3 @@ signupBtn.addEventListener("click", () => {
     removeStyle(loginBtn);
     addStyle(signupBtn);
 });
-
-
-
-const inputs = document.getElementsByTagName("input");
-
-for(let i = 0; i < inputs.length - 1; i++){
-    inputs[i].value = "";
-
-    inputs[i].addEventListener("input", () => {
-        if(inputs[i].value !== ""){
-            const label = inputs[i].parentNode.childNodes[1];
-            label.classList.add("label-out-input");
-            label.classList.remove("label-in-input");
-        }
-        if(inputs[i].value === ""){
-            const label = inputs[i].parentNode.childNodes[1];
-            label.classList.add("label-in-input");
-            label.classList.remove("label-out-input");
-        }
-    })
-}
