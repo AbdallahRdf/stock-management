94039b63242af8f583d3ceee85ea0f6839717971
abdallah <abdallahradfi@gmail.com>
2023-11-22 22:19:20 +0100

    fixed some code errors, added a Person controller

--- controllers/ClientController.php
+++ controllers/ClientController.php
@@ -5,47 +5,10 @@ require_once "../app/util/functions.php";
 //* requiring the autoloader
 require_once "../app/autoloader/autoloader.php";
 
-use App\Core\Validator;
 use App\Models\Client;
+use Controllers\PersonController;
 
 session_start();
-
-// this function redirects back to products page
-function goback()
-{
-    //* redirect to clients page;
-    header("Location: ../resources/views/pages/clients.php");
-    die();
-}
-
-// this function checks if the inputs are valid if not then send back an error message
-function handle_inputs_validation($name, $email, $phone_number, $date)
-{
-    $ERRORS = []; // will hold error messages
-    $OLD = []; // will hold old inputs data when there is an error;
-
-    if (!Validator::isStrValid($name)) {
-        $ERRORS["name_error"] = "Invalid name";
-    }
-    if (!Validator::isEmailValid($email)) {
-        $ERRORS["email_error"] = "Invalid email";
-    }
-    if (!preg_match("/^\d{10}$|^\d{3}-\d{3}-\d{4}$|^\(\d{3}\)\s?\d{3}-\d{4}$/", $phone_number)) {
-        $ERRORS["number_error"] = "Invalid phone number";
-    }
-    if (!empty($ERRORS)) // if there is errors
-    {
-        $OLD["old_name"] = $name;
-        $OLD["old_email"] = $email;
-        $OLD["old_number"] = $phone_number;
-        $OLD["old_date"] = $date;
-
-        $_SESSION["errors"] = $ERRORS;
-        $_SESSION["old"] = $OLD;
-
-        goback();
-    }
-}
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") // did we get to this page through a post request
 {
@@ -55,6 +18,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") // did we get to this page through a 
         $email = trim($_POST["email"]);
         $phone_number = trim($_POST["phoneNumber"]);
         $date = $_POST["date"];
+
+        PersonController::handle_inputs_validation($name, $email, $phone_number, $date, "clients");
 
         $result = Client::create($name, $email, $phone_number, $date);
 
@@ -73,6 +38,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") // did we get to this page through a 
         $phone_number = trim($_POST["phoneNumber"]);
         $date = $_POST["date"];
 
+        PersonController::handle_inputs_validation($name, $email, $phone_number, $date, "clients");
+
         $result = Client::update($client_id, $name, $email, $phone_number, $date);
 
         create_alert_session_variable("updated_successfully_alert", "Record Updated successfully!");
@@ -81,5 +48,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") // did we get to this page through a 
 
 $_SESSION["clients"] = Client::all();
 
-//* redirect to categories page;
-goback();
+//* redirect to clients page;
+header("Location: ../resources/views/pages/clients.php");
+die();
---
+++ controllers/PersonController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Controllers;
+
+//* requiring the autoloader
+require_once "../app/autoloader/autoloader.php";
+
+use App\Core\Validator;
+
+class PersonController
+{
+    // this function checks if the inputs are valid if not then send back an error message
+    public static function handle_inputs_validation($name, $email, $phone_number, $date, $view)
+    {
+        $ERRORS = []; // will hold error messages
+        $OLD = []; // will hold old inputs data when there is an error;
+
+        if (!Validator::isStrValid($name)) {
+            $ERRORS["name_error"] = "Invalid name";
+        }
+        if (!Validator::isEmailValid($email)) {
+            $ERRORS["email_error"] = "Invalid email";
+        }
+        if (!preg_match("/^\d{10}$|^\d{3}-\d{3}-\d{4}$|^\(\d{3}\)\s?\d{3}-\d{4}$/", $phone_number)) {
+            $ERRORS["number_error"] = "Invalid phone number";
+        }
+        if (!empty($ERRORS)) // if there is errors
+        {
+            $OLD["old_name"] = $name;
+            $OLD["old_email"] = $email;
+            $OLD["old_number"] = $phone_number;
+            $OLD["old_date"] = $date;
+
+            $_SESSION["errors"] = $ERRORS;
+            $_SESSION["old"] = $OLD;
+
+            //* redirect to clients page;
+            header("Location: ../resources/views/pages/{$view}.php");
+            die();
+        }
+    }
+}
