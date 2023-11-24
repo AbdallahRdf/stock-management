c82532a7cac71de36166fcbe5c0a88c4e832aa75
abdallah <abdallahradfi@gmail.com>
2023-11-24 17:54:56 +0100

    fixed the sidebar in the dashboard page

--- resources/views/pages/dashboard.php
+++ resources/views/pages/dashboard.php
@@ -14,9 +14,16 @@ if (!isset($_SESSION['user'])) // if not logged in redirect back to login page
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>InventoSync - Dashboard</title>
     <link rel="stylesheet" href="../../styles/sidebar.css">
+    <link rel="stylesheet" href="../../styles/overlay.css">
+    <link rel="stylesheet" href="../../styles/dropdown.css">
 </head>
 
 <body>
+    <!-- overlay -->
+    <div id="overlay" class="overlay"></div>
+
+    <?php require_once "../components/dropdown.php"; ?>
+
     <div class="container">
         <?php require_once "../components/sidebar.php"; ?>
 
@@ -26,6 +33,7 @@ if (!isset($_SESSION['user'])) // if not logged in redirect back to login page
     </div>
 
     <script src="../../js/sidebar.js"></script>
+    <script src="../../js/dropdown.js"></script>
 </body>
 
 </html>
