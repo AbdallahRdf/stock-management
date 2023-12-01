0f2764781d28f96a6ac9044be875b2ee491bbb5f
abdallah <abdallahradfi@gmail.com>
2023-12-01 23:20:11 +0100

    handled the empty table case

--- resources/views/components/table.php
+++ resources/views/components/table.php
@@ -13,16 +13,19 @@ $description_index = array_search("Description", $table_header); // get the posi
         <button id="add-btn" class="add-btn"> <!-- button to add new elements to the table -->
             <img src="../../img/plus-svgrepo-com.svg" alt="plus icon"> Add
         </button>
-        <div>
-            Show
-            <select name="limit" id="limit">
-                <option value="10">10</option>
-                <option value="25">25</option>
-                <option value="50">50</option>
-                <option value="100">100</option>
-            </select>
-            entries
-        </div>
+
+        <?php if (count($items) > 0): ?>
+            <div>
+                Show
+                <select name="limit" id="limit">
+                    <option value="10">10</option>
+                    <option value="25">25</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                </select>
+                entries
+            </div>
+        <?php endif ?>
     </div>
     <table id="table">
         <thead>
@@ -35,56 +38,66 @@ $description_index = array_search("Description", $table_header); // get the posi
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($items as $item): ?>
-                <!-- if the current element in the loop is the last item in the array then add the last class; because we want each td to have a border-bottom except for the td in the last tr of the table -->
-                <?php $class = $last_element === $item ? "last" : ""; ?>
-                <tr>
-                    <?php
-                    $indexed_item = array_values($item); // turn the assoc array into the indexed  array
-                    array_shift($indexed_item); // removing the first element which is the id, we are not going to show the id of each record in the table;
-                    ?>
-                    <?php for ($i = 0; $i < count($indexed_item); $i++): ?>
-                        <!-- if the current iteration is showing the description then put the description in a hidden <td> to use it later and skip the currenct iteration -->
-                        <?php if (!empty($description_index) && ($i === $description_index)): ?>
-                            <td class="<?= $class ?>" style="display:none">
+            <?php if (count($items) > 0): ?>
+                <?php foreach ($items as $item): ?>
+                    <!-- if the current element in the loop is the last item in the array then add the last class; because we want each td to have a border-bottom except for the td in the last tr of the table -->
+                    <?php $class = $last_element === $item ? "last" : ""; ?>
+                    <tr>
+                        <?php
+                        $indexed_item = array_values($item); // turn the assoc array into the indexed  array
+                        array_shift($indexed_item); // removing the first element which is the id, we are not going to show the id of each record in the table;
+                        ?>
+                        <?php for ($i = 0; $i < count($indexed_item); $i++): ?>
+                            <!-- if the current iteration is showing the description then put the description in a hidden <td> to use it later and skip the currenct iteration -->
+                            <?php if (!empty($description_index) && ($i === $description_index)): ?>
+                                <td class="<?= $class ?>" style="display:none">
+                                    <p>
+                                        <?= htmlspecialchars($indexed_item[$i]) ?>
+                                    </p>
+                                </td>
+                                <?php continue; endif; ?>
+
+                            <td class="<?= $class ?>">
                                 <p>
                                     <?= htmlspecialchars($indexed_item[$i]) ?>
                                 </p>
                             </td>
-                            <?php continue; endif; ?>
 
+                        <?php endfor; ?>
                         <td class="<?= $class ?>">
-                            <p>
-                                <?= htmlspecialchars($indexed_item[$i]) ?>
-                            </p>
-                        </td>
-
-                    <?php endfor; ?>
-                    <td class="<?= $class ?>">
-                        <?php if (in_array($current_view, ["products", "orders"])): ?>
-                            <button class="info-btn" id="info-btn" value="<?= $item['id'] ?>" title="info">
-                                <img src="../../img/info.svg" alt="info icon">
+                            <?php if (in_array($current_view, ["products", "orders"])): ?>
+                                <button class="info-btn" id="info-btn" value="<?= $item['id'] ?>" title="info">
+                                    <img src="../../img/info.svg" alt="info icon">
+                                </button>
+                            <?php endif; ?>
+                            <button class="modify-btn" id="modify-btn" value="<?= $item['id'] ?>" title="modify">
+                                <img src="../../img/update.svg" alt="modify icon">
                             </button>
-                        <?php endif; ?>
-                        <button class="modify-btn" id="modify-btn" value="<?= $item['id'] ?>" title="modify">
-                            <img src="../../img/update.svg" alt="modify icon">
-                        </button>
-                        <button class="delete-btn" id="delete-btn" value="<?= $item['id'] ?>" title="delete">
-                            <img src="../../img/delete.svg" alt="delete icon">
-                        </button>
+                            <button class="delete-btn" id="delete-btn" value="<?= $item['id'] ?>" title="delete">
+                                <img src="../../img/delete.svg" alt="delete icon">
+                            </button>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <tr>
+                    <td class="last" colspan="<?= count($table_header) ?>">
+                        <p class="table-empty-td">No Data To Show!</p>
                     </td>
                 </tr>
-            <?php endforeach; ?>
+            <?php endif ?>
         </tbody>
     </table>
     <!-- pagination -->
-    <div class="pagination">
-        <button class="pagination-btn previous" id="previous">
-            <img src="../../img/less-than.svg" alt="previous icon">
-        </button>
-        <p class="pagination-btn" id="page">1</p>
-        <button class="pagination-btn next" id="next">
-            <img src="../../img/greater-than.svg" alt="next icon">
-        </button>
-    </div>
+    <?php if (count($items) > 0): ?>
+        <div class="pagination">
+            <button class="pagination-btn previous" id="previous">
+                <img src="../../img/less-than.svg" alt="previous icon">
+            </button>
+            <p class="pagination-btn" id="page">1</p>
+            <button class="pagination-btn next" id="next">
+                <img src="../../img/greater-than.svg" alt="next icon">
+            </button>
+        </div>
+    <?php endif ?>
 </div>
