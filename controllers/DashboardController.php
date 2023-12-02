06bca394460eb1e408557c64630721104bdee94a
abdallah <abdallahradfi@gmail.com>
2023-12-02 23:05:02 +0100

    dashboard

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Models\Category;
+use App\Models\Client;
+use App\Models\Product;
+use App\Models\Supplier;
+
 require_once "../app/util/functions.php";
 
 //* requiring the autoloader
 require_once "../app/autoloader/autoloader.php";
 
 session_start();
+
+$_SESSION["client_count"] = number_format(count(Client::all()));
+$_SESSION["supplier_count"] = number_format(count(Supplier::all()));
+$_SESSION["category_count"] = number_format(count(Category::all()));
+$_SESSION["product_count"] = number_format(count(Product::all()));
+
+header("location: ../resources/views/pages/dashboard.php");
+die();
--- resources/views/pages/dashboard.php
+++ resources/views/pages/dashboard.php
@@ -4,25 +4,25 @@ require_once "../components/session_start.php"; // if not logged in redirect bac
 $cards_data = [
     [
         "card_title" => "Clients Count",
-        "card_count" => "2,480",
+        "card_count" => $_SESSION["client_count"],
         "card_icon" => "client-count.svg",
         "color" => "#4F73DE"
     ],
     [
         "card_title" => "Suppliers Count",
-        "card_count" => "28",
+        "card_count" => $_SESSION["supplier_count"],
         "card_icon" => "supplier-count.svg",
         "color" => "#1FC88C"
     ],
     [
         "card_title" => "Categories Count",
-        "card_count" => "124",
+        "card_count" => $_SESSION["category_count"],
         "card_icon" => "category-count.svg",
         "color" => "#36B9CB"
     ],
     [
         "card_title" => "Products Count",
-        "card_count" => "1,324",
+        "card_count" => $_SESSION["product_count"],
         "card_icon" => "products-count.svg",
         "color" => "#F5C138"
     ],
@@ -53,6 +53,8 @@ $cards_data = [
 
         <div class="main">
             <h2>Dashboard</h2>
+
+            <!-- cards -->
             <div class="cards">
                 <?php foreach($cards_data as $card): ?>
                     <div class="card" style="border-left-color: <?= $card["color"] ?>">
@@ -66,11 +68,17 @@ $cards_data = [
                     </div>
                 <?php endforeach ?>
             </div>
+
+            <!-- orders chart -->
+            <div>
+                <canvas id="orders-chart"></canvas>
+            </div>
         </div>
     </div>
 
     <script src="../../js/sidebar.js"></script>
     <script src="../../js/dropdown.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </body>
 
 </html>
