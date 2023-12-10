c02dde5d9132aa6c5b7dffbd9b2ce7c96bcedc57
abdallah <abdallahradfi@gmail.com>
2023-12-11 00:23:12 +0100

    update product info page

--- resources/views/pages/productsInfo.php
+++ resources/views/pages/productsInfo.php
@@ -3,8 +3,6 @@ require "../../../app/util/functions.php";
 require_once "../components/session_start.php"; // if not logged in redirect back to login page;
 
 $product = $_SESSION["product"][0];
-
-//dd($product);
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +13,7 @@ $product = $_SESSION["product"][0];
 
 <body>
     <!-- overlay -->
-    <div id="nameoverlay" class="overlay"></div>
+    <div id="overlay" class="overlay"></div>
 
     <div class="container">
         <!-- sidebar -->
@@ -24,36 +22,51 @@ $product = $_SESSION["product"][0];
         <div class="main">
 
             <!-- alert -->
-            <?php require_once "../components/alert.php"; ?>
+            <?php //require_once "../components/alert.php"; ?>
 
             <!-- card showing info -->
             <div class="product-container">
-                <h1><?= $product['name'] ?></h1>
-                <div class="element-grp">
-                    <label for="description">Description : </label>
-                    <p id="description"> <?= $product['description'] ?></p>
+                <div class="header">
+                    <a href="../../../controllers/ProductController.php">
+                        <img src="../../img/arrow.svg" alt="">
+                    </a>
+                    <h2>
+                        <?= htmlspecialchars($product['name']) ?>
+                    </h2>
                 </div>
                 <div class="element-grp">
-                    <label for="pprice"> Purchase Price : </label> <span id="pprice"><?= $product['purchase_price'] ?> </span>
+                    <label for="description">Description: </label>
+                    <textarea id="description" rows="6" disabled>
+                        <?= htmlspecialchars($product['description']) ?>
+                    </textarea>
                 </div>
                 <div class="element-grp">
-                    <label for="sprice"> Selling Price : </label> <span id="sprice"><?= $product['selling_price'] ?> </span>
+                    <label for="pprice"> Purchase Price: </label>
+                    <input type="text" id="pprice" value="<?= htmlspecialchars($product['purchase_price']) ?>" disabled>
                 </div>
                 <div class="element-grp">
-                    <label for="supplier"> Supplier : </label> <span id="supplier"><?= $product['supplier_name'] ?></span>
+                    <label for="sprice"> Selling Price: </label>
+                    <input type="text" id="sprice" value="<?= htmlspecialchars($product['selling_price']) ?>" disabled>
                 </div>
                 <div class="element-grp">
-                    <label for="category"> Category : </label> <span id="category"><?= $product['category_name'] ?></span>
+                    <label for="supplier"> Supplier: </label>
+                    <input type="text" id="supplier" value="<?= htmlspecialchars($product['supplier_name']) ?>"
+                        disabled>
                 </div>
                 <div class="element-grp">
-                    <label for="stock"> Stock Quantity : </label> <span id="stock"><?= $product['stock_quantity'] ?> </span>
+                    <label for="category"> Category: </label>
+                    <input type="text" id="category" value="<?= $product['category_name'] ?>" disabled>
+                </div>
+                <div class="element-grp">
+                    <label for="stock"> Stock Quantity: </label>
+                    <input type="text" id="stock" value="<?= $product['stock_quantity'] ?>" disabled>
                 </div>
 
             </div>
         </div>
 
         <script src="../../js/sidebar.js"></script>
-        <script src="../../js/alert.js"></script>
+        <!-- <script src="../../js/alert.js"></script> -->
         <script src="../../js/dropdown.js"></script>
 </body>
 
