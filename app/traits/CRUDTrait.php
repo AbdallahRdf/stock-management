e2b0f2555626721f5ac204260e4954014c881050
abdallah <abdallahradfi@gmail.com>
2024-01-01 02:24:45 +0100

    upated the crud trait

--- app/traits/CRUDTrait.php
+++ app/traits/CRUDTrait.php
@@ -6,13 +6,15 @@ use App\Core\Database;
 
 trait CRUDTrait
 {
-    public static function all($id = null)
+    private static function get_query()
     {
         if (in_array(static::TABLE_NAME, ["categories", "clients", "suppliers"])) {
             $sql = "SELECT "
                 . implode(", ", static::COLUMNS_TO_SHOW)
-                . " FROM " . static::TABLE_NAME . " ORDER BY created_at DESC;";
-        } else if (static::TABLE_NAME === "products") {
+                . " FROM " . static::TABLE_NAME . " ORDER BY created_at DESC";
+        } 
+        else if (static::TABLE_NAME === "products") 
+        {
             $sql = "SELECT 
                 products.id, 
                 products.name, 
@@ -26,72 +28,45 @@ trait CRUDTrait
             FROM products 
             JOIN categories ON products.category_id = categories.id 
             JOIN suppliers ON products.supplier_id = suppliers.id  
-            ORDER BY products.created_at DESC;";
-        } else if (in_array(static::TABLE_NAME, ["clientOrders", "supplierOrders"])) {
+            ORDER BY products.created_at DESC";
+        } 
+        else if (in_array(static::TABLE_NAME, ["clientOrders", "supplierOrders"])) 
+        {
             $sql = "SELECT 
                 " . static::TABLE_NAME . ".id, 
                 " . static::TABLE_NAME . ".date, 
                 " . static::TABLE_TO_JOIN . ".full_name as name 
             FROM " . static::TABLE_NAME . " JOIN " . static::TABLE_TO_JOIN . " 
             WHERE " . static::TABLE_NAME . "." . static::TRADE_PARTNER_ID . " = " . static::TABLE_TO_JOIN . ".id 
-            ORDER BY " . static::TABLE_NAME . ".date DESC;";
-        } else if (in_array(static::TABLE_NAME, ["clientOrderedProducts", "supplierOrderedProducts"])) {
+            ORDER BY " . static::TABLE_NAME . ".date DESC";
+        } 
+        else if (in_array(static::TABLE_NAME, ["clientOrderedProducts", "supplierOrderedProducts"])) 
+        {
             $sql = "SELECT 
                 " . static::TABLE_NAME . ".id, 
                 products.name as product_name,
                 " . static::TABLE_NAME . ".quantity
             FROM " . static::TABLE_NAME . " JOIN products 
             WHERE " . static::TABLE_NAME . ".product_id = products.id and " . static::TABLE_NAME . ".order_id=:order_id
-            Order BY " . static::TABLE_NAME . ".created_at DESC;";
-
-            $params = [':order_id' => $id];
+            Order BY " . static::TABLE_NAME . ".created_at DESC";
         }
+        return $sql;
+    }
 
-        return (new Database)->query($sql, $params ?? null);
+    public static function all($id = null)
+    {
+        $params = ($id !== null) ? [':order_id' => $id] : null;
+
+        return (new Database)->query(static::get_query(), $params);
     }
 
     public static function paginate($offset = 0, $limit = 10, $id = null)
     {
-        if (in_array(static::TABLE_NAME, ["categories", "clients", "suppliers"])) {
-            $sql = "SELECT "
-                . implode(", ", static::COLUMNS_TO_SHOW)
-                . " FROM " . static::TABLE_NAME . " ORDER BY created_at DESC LIMIT $limit OFFSET $offset;";
-        } else if (static::TABLE_NAME === "products") {
-            $sql = "SELECT 
-                products.id, 
-                products.name, 
-                products.description,
-                products.excerpt, 
-                products.purchase_price, 
-                products.selling_price,
-                products.stock_quantity,
-                suppliers.full_name as supplier_name,
-                categories.name as category_name 
-            FROM products 
-            JOIN categories ON products.category_id = categories.id 
-            JOIN suppliers ON products.supplier_id = suppliers.id  
-            ORDER BY products.created_at DESC LIMIT $limit OFFSET $offset;";
-        } else if (in_array(static::TABLE_NAME, ["clientOrders", "supplierOrders"])) {
-            $sql = "SELECT 
-                " . static::TABLE_NAME . ".id, 
-                " . static::TABLE_NAME . ".date, 
-                " . static::TABLE_TO_JOIN . ".full_name as name 
-            FROM " . static::TABLE_NAME . " JOIN " . static::TABLE_TO_JOIN . " 
-            WHERE " . static::TABLE_NAME . "." . static::TRADE_PARTNER_ID . " = " . static::TABLE_TO_JOIN . ".id 
-            ORDER BY " . static::TABLE_NAME . ".date DESC LIMIT $limit OFFSET $offset;";
-        } else if (in_array(static::TABLE_NAME, ["clientOrderedProducts", "supplierOrderedProducts"])) {
-            $sql = "SELECT 
-                " . static::TABLE_NAME . ".id, 
-                products.name as product_name,
-                " . static::TABLE_NAME . ".quantity
-            FROM " . static::TABLE_NAME . " JOIN products 
-            WHERE " . static::TABLE_NAME . ".product_id = products.id and " . static::TABLE_NAME . ".order_id=:order_id
-            Order BY " . static::TABLE_NAME . ".created_at DESC LIMIT $limit OFFSET $offset;";
+        $sql = static::get_query() . " LIMIT $limit OFFSET $offset;";
 
-            $params = [':order_id' => $id];
-        }
+        $params = ($id !== null) ? [':order_id' => $id] : null;
 
-        return (new Database)->query($sql, $params ?? null);
+        return (new Database)->query($sql, $params);
     }
 
     public static function create($assoc_array)
